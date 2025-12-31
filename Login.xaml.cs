2aa944a313c6645486ef00685acd4154768bbadb
Luis Enrique Tovar Chavez <147081000+tovarcmg@users.noreply.github.com>
2025-12-30 22:10:22 -0600

    se realizan correcciones de login

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -12,28 +12,22 @@ public partial class Login : ContentPage
     private async void btnBio_Clicked(object sender, EventArgs e)
     {
         var result = await BiometricAuthenticationService.Default.AuthenticateAsync(
-
-        new AuthenticationRequest()
-        {
-            Title = "Biometric Request",
-            NegativeText = "Cancel authentication"
-        },
+            new AuthenticationRequest()
+            {
+                Title = "Biometric Request",
+                NegativeText = "Cancel authentication"
+            },
             CancellationToken.None
         );
 
         //// The following code block goes here
-        ///
         if (result.Status == BiometricResponseStatus.Success)
         {
             Navigation.PushAsync(new MainPage());
-
         }
         else
         {
             await DisplayAlert("Error", "Authentication failed.", "Ok");
         }
-
-
-
     }
 }
